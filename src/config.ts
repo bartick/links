3583007248003f02d4029d58f255f5d05bd48f2d
bartick <bartickmaiti1998@gmail.com>
2023-09-30 13:36:50 +0530

    init: Added some initial links

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+export const SUBTITLE = "A Backend Developer"
+export const TITLE = "Bartick Maiti"
+export const TAGLINE = "I am a passionate technology student specializing in backend development. I love to learn new things and solve problems. I am currently pursuing my B.Tech in Computer Science and Engineering."
+
+// Contacts
+export const contact = "Keep in touch"
+export const phone = "+918777867265"
+export const email = "contact@bartick.me"
